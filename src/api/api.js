2fba743f6a88ed2ca659883ed3758895eb5c627f
anjalikumari11 <anjalikumari38895@gmail.com>
2026-07-01 16:54:31 +0530

    minor changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.APP_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.APP_URL || 'https://studentms-j6yd.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
